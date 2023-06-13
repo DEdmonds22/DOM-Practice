960ec2aaac8dfd5f42b1742b8d2cd46dd4494f1d
DEdmonds22 <ddedmonds22@gmail.com>
2023-06-13 17:37:04 -0400

    Selecting DOM Elements Finished. Next Select a single element by its id

--- js/script.js
+++ js/script.js
@@ -7,3 +7,15 @@
         + Change the content of elements
         + Change the style properties of elements
 */
+
+// Selecting DOM Elements
+/*
+    - Web devs make web pages dynamic by manipulating the DOM. For ex:
+        + In a To Do app, the user types a new todo into an input, clicks a button and the new todo is added to the list.
+    - The above scenario requires the app's JS to:
+        + Attach an event listener to the button element
+        + Grab the text entered from the input element
+        + Create a new element, e.g. an li, and set it's content
+        + Append the new element to its parent
+    - Devs must use JS to select DOM elements so that the above steps can be performed.
+*/
