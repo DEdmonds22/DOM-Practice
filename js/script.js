3b8d37eb243650048931604cf328dfc1bdff7d1b
DEdmonds22 <ddedmonds22@gmail.com>
2023-06-13 17:14:20 -0400

    Inital Setup

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// WHAT'S THE DOM?
+/*
+    - The DOM (document object model) is the in-memory representation of a browser's web document.
+    - It's a tree-like data structure w/ the top (root) being the document object.
+    - The DOM's API (application programming interface) enables developers to make the UI dynaic by using JS to:
+        + Add/remove elements to/from the document
+        + Change the content of elements
+        + Change the style properties of elements
+*/
